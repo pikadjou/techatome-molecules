cfb5341cee97f5e47804281aebc3fe0ea883eed0
Geoffrey L <geoffrey@g-lambert.be>
2026-05-02 20:39:17 +0200

    fix

--- projects/form/form-input/src/lib/components/input/wysiswyg/wysiswyg.component.ts
+++ projects/form/form-input/src/lib/components/input/wysiswyg/wysiswyg.component.ts
@@ -1,20 +1,27 @@
-import { Component } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { InputWysiswyg } from "@ta/form-model";
-import { TranslatePipe } from "@ta/translation";
-import { EditorInputComponent, EditorInputSavedData } from "@ta/wysiswyg";
+import { InputWysiswyg } from '@ta/form-model';
+import { TaTranslationService } from '@ta/translation';
+import { EditorInputComponent, EditorInputSavedData } from '@ta/wysiswyg';
 
-import { TaAbstractInputComponent } from "../../abstract.component";
-import { InputLayoutComponent } from "../../input-layout/input-layout.component";
+import { TaAbstractInputComponent } from '../../abstract.component';
+import { InputLayoutComponent } from '../../input-layout/input-layout.component';
 
 @Component({
-  selector: "ta-input-wysiswyg",
-  templateUrl: "./wysiswyg.component.html",
-  styleUrls: ["./wysiswyg.component.scss"],
+  selector: 'ta-input-wysiswyg',
+  templateUrl: './wysiswyg.component.html',
+  styleUrls: ['./wysiswyg.component.scss'],
   standalone: true,
-  imports: [EditorInputComponent, InputLayoutComponent, ReactiveFormsModule, TranslatePipe],
+  imports: [EditorInputComponent, InputLayoutComponent, ReactiveFormsModule],
 })
+export class WysiswygComponent extends TaAbstractInputComponent<InputWysiswyg> {
+  private _translationService = inject(TaTranslationService);
+
+  get translatedPlaceholder(): string | undefined {
+    const key = this.input.placeholder;
+    return key ? this._translationService.instant(key) : undefined;
+  }
   public set(value: EditorInputSavedData) {
     this.input.value = value.blocks;
   }
