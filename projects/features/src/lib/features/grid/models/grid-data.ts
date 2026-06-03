c9469d7e9cfdd7474dca7ee87d4c4ea0252909d7
Geoffrey L <geoffrey@g-lambert.be>
2026-06-03 20:48:10 +0200

    refactor(@ta/features): replace Tabulator instance with TaTableState in TaGridData

--- projects/features/src/lib/features/grid/models/grid-data.ts
+++ projects/features/src/lib/features/grid/models/grid-data.ts
@@ -1,7 +1,6 @@
-import { ElementRef, signal } from '@angular/core';
+import { signal } from '@angular/core';
 
-import { BehaviorSubject, Observable, Subject, firstValueFrom } from 'rxjs';
-import { ColumnDefinition, Options, TabulatorFull as Tabulator } from 'tabulator-tables';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 import { BaseCol } from './cols/base-col';
 import { BoolCol } from './cols/bool-col';
@@ -11,6 +10,7 @@ import { NumberCol } from './cols/number-col';
 import { RelationCol } from './cols/relation-col';
 import { StringCol } from './cols/string-col';
 import { TaGridFilters } from './grid-filters';
+import { TaTableState } from './table-state';
 import { ColMetaData, Filter, ParameterType, Preset, ViewType, ajaxRequestFuncParams, ajaxResponse } from './types';
 import { groupBy } from './utils';
 
@@ -30,14 +30,13 @@ export class TaGridData<T> {
   }
 
   public readonly rowClicked$ = new Subject<T>();
-  public table: Tabulator | null = null;
+  public table: TaTableState<T> | null = null;
   public cols: { [index: string]: BaseCol<any> } = {};
   public filters: TaGridFilters | null = null;
 
   public readonly isReady$ = new BehaviorSubject(false);
   public readonly isDataReady$ = new BehaviorSubject(false);
 
-  public tableHtml: ElementRef | null = null;
   public readonly displayType = signal<ViewType>('card');
 
   public groupBy: keyof T | null = null;
@@ -46,23 +45,32 @@ export class TaGridData<T> {
   constructor(public readonly scope: string) {}
 
   public init(params: {
-    elementRef: ElementRef;
     colsMetaData: ColMetaData[];
     data?: T[];
     services?: IDataService<T>;
     initialFilter?: Filter[];
     preset?: Preset[];
   }) {
-    this.table = new Tabulator(params.elementRef.nativeElement, this._getOptions(params));
+    this._buildCols(params.colsMetaData);
 
-    this.table.on('tableBuilt', () => {
-      this.tableHtml = params.elementRef;
-      this.filters = new TaGridFilters(this.scope, this.table!, params.preset);
-      this.isReady$.next(true);
+    this.table = new TaTableState<T>();
+    this.table.init({
+      colsMetaData: params.colsMetaData,
+      data: params.data,
+      services: params.services,
+      initialFilter: params.initialFilter,
+      onDataUpdate: total => this.totalItems.set(total),
     });
-    this.table.on('rowClick', (_e, row) => {
-      this.rowClicked$.next(<T>row.getData());
+
+    this.filters = new TaGridFilters(this.scope, this.table, params.preset);
+
+    this.table.isReady$.subscribe(ready => {
+      if (ready) this.isReady$.next(true);
     });
+    this.table.isDataReady$.subscribe(ready => {
+      if (ready) this.isDataReady$.next(true);
+    });
+    this.table.rowClicked$.subscribe(row => this.rowClicked$.next(row));
   }
 
   public destroy() {
@@ -72,78 +80,23 @@ export class TaGridData<T> {
   public setGroupBy(field: string) {
     this.groupBy = field as keyof T;
     this.table?.setGroupBy(field);
-    this.table?.setData();
   }
   public clearGroupBy() {
     this.groupBy = null;
-    this.table?.setGroupBy([]);
-    this.table?.setData();
+    this.table?.setGroupBy(null);
   }
 
   public switchView(type: ViewType) {
     this.displayType.set(type);
   }
 
-  private _getOptions(params: {
-    colsMetaData: ColMetaData[];
-    data?: T[];
-    services?: IDataService<T>;
-    initialFilter?: Filter[];
-  }): Options {
-    const baseOptions: Options = {
-      height: '500px',
-      layout: 'fitColumns',
-      initialFilter: params.initialFilter,
-      columns: this._getColumns(params.colsMetaData),
-    };
-
-    const serviceOptions: Options = params.services
-      ? {
-          paginationMode: 'remote',
-          pagination: true,
-          paginationSize: 20,
-          paginationInitialPage: 1,
-          ajaxFiltering: true,
-          filterMode: 'remote',
-          ajaxSorting: true,
-          sortMode: 'remote',
-          ajaxURL: 'dummy',
-          ajaxRequestFunc: (url: string, config: any, ajaxParams: ajaxRequestFuncParams): Promise<ajaxResponse<T>> => {
-            return firstValueFrom(
-              params.services!.getData$({
-                ...ajaxParams,
-                groupBy: this.groupBy as string,
-                colsMetaData: params.colsMetaData,
-              })
-            );
-          },
-          ajaxResponse: (_: any, __: any, response: ajaxResponse<T>) => {
-            this.isDataReady$.next(true);
-            this.totalItems.set(response.total);
-            return response;
-          },
-        }
-      : {};
-
-    const dataOptions: Options = params.data
-      ? {
-          data: params.data,
-        }
-      : {};
-    return { ...baseOptions, ...serviceOptions, ...dataOptions };
-  }
-
-  private _getColumns(colsMetaData: ColMetaData[]): ColumnDefinition[] {
+  private _buildCols(colsMetaData: ColMetaData[]): void {
     this.cols = Object.fromEntries(
       colsMetaData.map(meta => {
         const field = this._factoryCols(meta);
         return [field.key, field];
       })
     );
-
-    return Object.keys(this.cols)
-      .filter(key => !key.startsWith('_'))
-      .map(key => ({ ...this.cols[key].getColDef() }));
   }
 
   private _factoryCols(col: ColMetaData): BaseCol<any> {
